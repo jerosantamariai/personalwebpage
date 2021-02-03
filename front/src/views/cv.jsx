199765f8b0e4bba8a1e38dee9028838b463ad048
Jerónimo Santa María Illanes <jerosantamariai@MacBook-Pro-de-Jeronimo.local>
2021-02-03 20:12:44 -0300

    forgot on sprint 2 responsive for cv

--- front/src/views/cv.jsx
+++ front/src/views/cv.jsx
@@ -4,13 +4,17 @@ import CV_jerosantamariai from '../documents/CV_jerosantamariai.pdf';
 
 const CV = props => {
     return (
-        <div className="col-6 offset-3 d-flex cvtainer">
-            <div className="col-6">
-                <img className="JSMIFoto" src={JSMIFoto} alt="JSMIFoto" style={{width: 200}} />
-            </div>
-            <div className="col-6 my-auto">
-                <p className="d-flex justify-content-center">Puedes descargar mi CV aquí:</p>
-                <a className="btn btn-info d-flex justify-content-center" href={CV_jerosantamariai} download><i className="fas fa-cloud-download-alt fa-lg pt-1" />&nbsp;&nbsp;&nbsp;&nbsp;Descargar</a>
+        <div className="cvtainer">
+            <div className="row py-3 d-flex justify-content-center">
+                <div className="col-sm-12 col-md-4 py-5 d-flex justify-content-center">
+                    <img className="JSMIFoto" src={JSMIFoto} alt="JSMIFoto" style={{ width: 200 }} />
+                </div>
+                <div className="col-sm-12 col-md-6 my-auto pb-5 d-flex justify-content-center">
+                    <div className="col-8">
+                        <p className="d-flex justify-content-center">Puedes descargar mi CV aquí:</p>
+                        <a className="btn btn-info d-flex justify-content-center" href={CV_jerosantamariai} download><i className="fas fa-cloud-download-alt fa-lg pt-1" />&nbsp;&nbsp;&nbsp;&nbsp;Descargar</a>
+                    </div>
+                </div>
             </div>
         </div>
     );

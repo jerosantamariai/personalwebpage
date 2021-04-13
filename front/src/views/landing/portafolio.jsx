0095da6e9635077f7e53508e2bb977f71f47a551
jerosantamariai <jerosantamariai@gmail.com>
2021-04-12 20:04:37 -0400

    adding portafolio

---
+++ front/src/views/landing/portafolio.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Port = props => {
+    return(
+        <div className="portainer">
+            <h1>port</h1>
+        </div>
+    );
+}
+
+export default Port;

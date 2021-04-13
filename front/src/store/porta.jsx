2d6adddbbf68fe107fa49f0e91ca0a688e6efcb3
jerosantamariai <jerosantamariai@gmail.com>
2021-04-12 20:23:02 -0400

    adding images

---
+++ front/src/store/porta.jsx
@@ -0,0 +1,18 @@
+import PPyme from '../img/experience/protegepyme.png';
+import ABrucher from '../img/experience/andreabrucher.png';
+import CHube from '../img/experience/conihube.png';
+
+export default [
+    {
+        "img": PPyme,
+        "url": "http://www.protegepyme.com"
+    },
+    {
+        "img": ABrucher,
+        "url": "http://www.andreabrucher.cl"
+    },
+    {
+        "img": CHube,
+        "url": "http://www.constanzahube.cl"
+    }
+]
--- front/src/views/landing/portafolio.jsx
+++ front/src/views/landing/portafolio.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ABrucher from '../../img/experience/andreabrucher.png';
+import PPyme from '../../img/experience/protegepyme.png';
+import CHube from '../../img/experience/conihube.png';
 
 const Port = props => {
     return(
         <div className="portainer">
-            <h1>port</h1>
+            <h1>Han confiado en mi:</h1>
+            <div className="row">
+                <div className="col-12 text-center">
+                    <img src={ABrucher} alt="" className="portimg" />
+                    <img src={PPyme} alt="" className="portimg" />
+                    <img src={CHube} alt="" className="portimg" />
+                </div>
+            </div>
         </div>
     );
 }
